6004dbaaf86d2c6f87af21de48fde661426e9200
slowlyo <slowlyo_email@qq.com>
2025-02-07 20:46:49 +0800

    feat: amis update to 6.11.0

--- web/dist/assets/Calendar-BHpk7lKy.js
+++
@@ -1,1 +0,0 @@
-import{s as t,y as a,z as d,D as o}from"./index-Cuf9I9Av.js";import{DateControlRenderer as n}from"./InputDate-BEdzQL7F.js";var i=function(r){t(e,r);function e(){return r!==null&&r.apply(this,arguments)||this}return e.defaultProps=a(a({},n.defaultProps),{embed:!0}),e=d([o({type:"calendar"})],e),e}(n);export{i as CalendarRenderer};
