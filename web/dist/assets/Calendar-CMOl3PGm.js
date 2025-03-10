adb4f5595a30ebda597e50f3b674bdb77813a00b
slowlyo <slowlyo_email@qq.com>
2025-03-10 16:52:52 +0800

    amis回退6.10.0

--- web/dist/assets/Calendar-CMOl3PGm.js
+++
@@ -1,1 +0,0 @@
-import{s as t,y as a,z as d,D as o}from"./index-CpG9c5oN.js";import{DateControlRenderer as n}from"./InputDate-Cl3Pch9W.js";var i=function(r){t(e,r);function e(){return r!==null&&r.apply(this,arguments)||this}return e.defaultProps=a(a({},n.defaultProps),{embed:!0}),e=d([o({type:"calendar"})],e),e}(n);export{i as CalendarRenderer};
